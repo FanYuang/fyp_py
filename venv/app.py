9b2028283b824222e9fb0b4989ee58157dc87942
FanYuang <1155124596@link.cuhk.edu.hk>
2021-11-12 17:11:09 +0800

    c

--- venv/app.py
+++ venv/app.py
@@ -3,6 +3,7 @@ import random
 import numpy as np  
 import pymongo
 import sys
+from scipy.stats import norm
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 mydb = myclient["fyp"]
 mycol = mydb["data"]
@@ -35,27 +36,149 @@ def generatenum_normal(mean, standard_d, num_nor):
     return array
 
 def binarySearch (arr, l, r, x): 
-  
-    
     if r >= l: 
-  
         mid = int(l + (r - l)/2)
-  
-        
         if arr[mid] == x: 
             return mid 
-          
-       
         elif arr[mid] > x: 
-            return binarySearch(arr, l, mid-1, x) 
-  
-       
+            return binarySearch(arr, l, mid-1, x)
         else: 
             return binarySearch(arr, mid+1, r, x) 
-  
     else: 
-        
         return -1
+
+class TreeNode(object): 
+    def __init__(self,_val): 
+        self.val = _val 
+        self.left = None
+        self.right = None
+        self.height = 1
+        
+class AVL_Tree(object): 
+    def insert(self, root, val): 
+               
+        #Simple Bst Insertion:
+        if not root: 
+            return TreeNode(val) 
+        elif val < root.val: 
+            root.left = self.insert(root.left, val) 
+        else: 
+            root.right = self.insert(root.right, val)
+       
+        # 2)modify the height      
+        root.height = 1 + max(self.Height(root.left), self.Height(root.right)) 
+        # 3)Get the Balancing Factor
+        balance = self.check_Avl(root) 
+        # 4)Balance The tree using required set of rotation
+        
+        #RR Rotation as tree is Left Skewed
+        if balance > 1 and val < root.left.val: 
+            return self.RR(root) 
+
+        #LL Rotation as tree is Right Skewed
+        if balance < -1 and val > root.right.val: 
+            return self.LL(root) 
+        #RL Rotation as tree is Left then Right Skewed
+        if balance > 1 and val > root.left.val: 
+            root.left = self.LL(root.left) 
+            return self.RR(root) 
+        #LR Rotation as tree is Right then Left Skewed
+        if balance < -1 and val < root.right.val: 
+            root.right = self.RR(root.right) 
+            return self.LL(root) 
+  
+        return root 
+     #LL Rotation
+    def LL(self, node): 
+       
+        p = node.right 
+        t = p.left
+        #Rotations:
+        p.left = node 
+        node.right = t 
+        #modify the heights: 
+        node.height = 1 + max(self.Height(node.left), self.Height(node.right)) 
+        p.height = 1 + max(self.Height(p.left), self.Height(p.right)) 
+   
+        return p 
+    #LL Rotation
+    def RR(self, node): 
+  
+        p = node.left 
+        t = p.right
+        #Rotations:
+        p.right = node
+        node.left = t 
+        #modify the heights:
+        node.height = 1 + max(self.Height(node.left), self.Height(node.right)) 
+        p.height = 1 + max(self.Height(p.left), self.Height(p.right)) 
+        return p 
+    #Getting the Height
+    def Height(self, root): 
+        if not root: 
+            return 0
+  
+        return root.height 
+    #Getting the Balancing Factor
+    def check_Avl(self, root): 
+        if not root: 
+            return 0
+  
+        return self.Height(root.left) - self.Height(root.right) 
+  
+    def preOrder(self, root): 
+  
+        if not root: 
+            return
+  
+        print("{0} ".format(root.val), end="") 
+        self.preOrder(root.left) 
+        self.preOrder(root.right) 
+
+
+def insert_data(_data):
+        mytree = AVL_Tree()
+        root = None
+        for i in _data:
+            root = mytree.insert(root,i)
+        print("Preorder Traversal of constructed AVL tree is:")
+        mytree.preOrder(root)
+        print()
+        return root
+
+def Search(root,val):
+    if (root is None):
+        return False
+    elif (root.val == val):
+        return True
+    elif(root.val < val):
+        return Search(root.right,val)
+    return Search(root.left,val)
+    return False
+
+
+def tricksearch(set, index, target):
+    if (index<0 or index>len(set)-1):
+        return -1
+    elif  (set[index] < target):
+        while (set[index]<target):
+            index+=1
+        if (set[index]==target):
+            return index
+        else:
+            return -1
+    elif (set[index]>target):
+        while (set[index]<target):
+            index+=1
+        if (set[index]==target):
+            return index
+        else:
+            return -1
+
+
+
+    
+
 
 @app.route("/")
 def hello_world():
@@ -86,7 +209,7 @@ def hashtable_uniform():
     size=sys.getsizeof(dict)
     print(size)
     for num in testset:
-        index=dict.get(num)
+        result=dict.get(num)
     
     return "<p>Heiheihei</p>"
 
@@ -98,16 +221,60 @@ def hashtable_normal():
     size=sys.getsizeof(dict)
     print(size)
     for num in testset:
-        index=dict.get(num)
+        result=dict.get(num)
     return "<p>Heiheihei</p>"
 
 @app.route("/binarysearch_uniform")
 def binarysearch_uniform():
-    
+    index=sorted(dataset)
+    size=sys.getsizeof(index)
+    print(size)
+    for num in testset:
+        result= binarySearch(index, 0, len(index)-1, num)
     return "<p>Heiheihei</p>"
 
 
 @app.route("/binarysearch_normal")
 def binarysearch_normal():
-    
+    index=sorted(dataset)
+    size=sys.getsizeof(index)
+    print(size)
+    for num in testset:
+        result= binarySearch(index, 0, len(index)-1, num)
+    return "<p>Heiheihei</p>"
+
+@app.route("/avl_uniform")
+def avl_uniform():
+    root = insert_data(testset)
+    size=sys.getsizeof(root)
+    for num in testset:
+        result=Search(root,num)
+    return "<p>Heiheihei</p>"
+
+
+@app.route("/avl_normal")
+def avl_normal():
+    root = insert_data(testset)
+    size=sys.getsizeof(root)
+    for num in testset:
+        result=Search(root,num)
+    return "<p>Heiheihei</p>"
+
+@app.route("/trick_uniform")
+def trick_uniform():
+    index=sorted(dataset)
+    size=sys.getsizeof(index)
+    print(size)
+    for i,num in enumerate(testset):
+        result=tricksearch(index, round(num_uni * (num - low) / (high - low)), num)
+    return "<p>Heiheihei</p>"
+
+
+@app.route("/trick_normal")
+def trick_normal():
+    index=sorted(dataset)
+    size=sys.getsizeof(index)
+    print(size)
+    for i,num in enumerate(testset):
+        result=tricksearch(index, round(num_nor * norm.cdf(num,mean,standard_d)), num)
     return "<p>Heiheihei</p>"
